7d3240275d4371f0bfbe995f58fbaf435b5d704a
Inaboya Collins <inaboyacollins@icloud.com>
2022-09-12 08:44:44 +0000

    File Committed

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n = '0';
+	char l = 'a';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+	while (l <= 'f')
+        {
+                putchar(l);
+                l++;
+        }
+	putchar('\n');
+	return (0);
+}
