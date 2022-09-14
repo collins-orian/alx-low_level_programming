f3cb591cfba4200adcd78cd239af346c6807b078
Oshioregbeme Collins Inaboya <111002610+collins-orian@users.noreply.github.com>
2022-09-14 15:24:20 +0100

    File Committed

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,12 @@
 #include "main.h"
 
 /**
- * print_alphabet 
-*/
+ * print_alphabet
+ *
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
+ */
 
 void print_alphabet(void)
 {
