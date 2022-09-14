eb6423566ec38a44f86ab000054d13da66f6ff85
Oshioregbeme Collins Inaboya <111002610+collins-orian@users.noreply.github.com>
2022-09-14 15:18:28 +0100

    File Committed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
+#include "main.h"
 #include <string.h>
-#include "_putchar.h"
 
 /**
  * main - Entry point
@@ -7,16 +7,16 @@
  * Description: prints _putchar using putchar prototype
  *
  * Return: Always 0 (Success)
- */
+*/
 
 int main(void)
 {
-    char str[] = "_putchar";
-    int ch;
+	char str[] = "_putchar";
+	int ch;
 
-    for (ch = 0; ch < 8; ++ch)
-        _putchar(str[ch]);
-    _putchar('\n');
+	for (ch = 0; ch < 8; ++ch)
+		_putchar(str[ch]);
+	_putchar('\n');
 
-    return 0;
+	return (0);
 }
