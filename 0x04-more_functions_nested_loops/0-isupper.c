2396708c3e2b1ee194e1358b3bae7f687d5f2acf
Oshioregbeme Collins Inaboya <111002610+collins-orian@users.noreply.github.com>
2022-09-16 12:29:01 +0100

    File Committed

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include "main.h"
-#include <ctype.h>
 
 /**
-* main - tests if letters are upper case letters
-* Return: 0
-*/
+ * main - tests if letters are upper case letters
+ *
+ * Return: 0
+ *
+ * */
 
 int _isupper(int c)
 {
