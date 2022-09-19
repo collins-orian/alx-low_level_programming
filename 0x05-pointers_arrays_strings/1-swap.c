ebce424e013534f7b5aebace1e852544d511249a
Oshioregbeme Collins Inaboya <111002610+collins-orian@users.noreply.github.com>
2022-09-19 20:34:13 +0100

    Uploaded file for Pointers and Arrays

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * reset_value - function that resets the integer value
+ * reset_to_98 - function that resets the integer value
  * @n: the parameter to be reset
  * Descrption - this function resets the value of an integer to 98
  */
 
-void reset_value(int *n)
+void reset_to_98(int *n)
 {
 	*n = 98
 }
