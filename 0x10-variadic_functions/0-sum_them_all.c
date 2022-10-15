4751eeb332de25d56541f13b33fe87664aaf93a6
Oshioregbeme Collins Inaboya <111002610+collins-orian@users.noreply.github.com>
2022-10-15 23:19:52 +0100

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,14 +7,14 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-        unsigned int i, sum = 0;
-        va_list arguments;
+	unsigned int i, sum = 0;
+	va_list arguments;
 
-        va_start(arguments, n);
+	va_start(arguments, n);
 
-        for (i = 0; i < n; i++)
-                sum = sum + va_arg(arguments, int);
+	for (i = 0; i < n; i++)
+		sum = sum + va_arg(arguments, int);
 
-        va_end(arguments);
-        return (sum);
+	va_end(arguments);
+	return (sum);
 }
