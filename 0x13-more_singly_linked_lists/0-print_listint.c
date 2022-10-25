0dd5dfab6bc944b45d82cc78757040279dfb5d7f
Oshioregbeme Collins Inaboya <111002610+collins-orian@users.noreply.github.com>
2022-10-25 13:13:11 +0100

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,9 +1,12 @@
 #include "lists.h"
 /**
  * print_listint - prints all the elements of a listint_t list.
+ * 
  * @h: pointer to the list header address
+ * 
  * Return: number of nodes
  */
+
 size_t print_listint(const listint_t *h)
 {
         size_t i;
