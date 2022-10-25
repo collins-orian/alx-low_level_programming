7315d7d150598807207c80289c24c108a41ae81f
Oshioregbeme Collins Inaboya <111002610+collins-orian@users.noreply.github.com>
2022-10-25 14:21:58 +0100

    Updates

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,17 +9,16 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node;
 
-	/* In case it isn't imput a head*/
 	if (head == NULL)
 		return (NULL);
-	/*Allocate the new node*/
+
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
-	/*Fill the new node*/
+
 	new_node->n = n;
 	new_node->next = *head;
-	/*Assign the new node as head*/
+
 	*head = new_node;
 
 	return (new_node);
