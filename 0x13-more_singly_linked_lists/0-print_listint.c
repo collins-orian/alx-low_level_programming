73e1576c025a9e7930bfd98ec53b29e5def11b7e
Oshioregbeme Collins Inaboya <111002610+collins-orian@users.noreply.github.com>
2022-10-25 13:21:42 +0100

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,19 +10,19 @@
 
 size_t print_listint(const listint_t *h)
 {
-        size_t i;
-        const listint_t *temp;
+	size_t i;
+	const listint_t *temp;
 
-        i = 0;
-        if (h == NULL)
-                return (0);
-        temp = h;
+	i = 0;
+	if (h == NULL)
+		return (0);
+	temp = h;
 
-        while (temp != NULL)
-        {
-                printf("%d\n", temp->n);
-                temp = temp->next;
-                i++;
-        }
-        return (i);
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		i++;
+	}
+	return (i);
 }
