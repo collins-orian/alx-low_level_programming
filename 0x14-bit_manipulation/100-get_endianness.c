118ccce29050aed808604be1a12dc783b34359ba
Oshioregbeme Collins Inaboya <111002610+collins-orian@users.noreply.github.com>
2022-10-27 21:40:22 +0100

    Update 100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -5,10 +5,10 @@
  */
 int get_endianness(void)
 {
-        int i = 1;
+	int i = 1;
 
-        if ((i >> 31) == 1)
-                return (0);
-        else
-                return (1);
+	if ((i >> 31) == 1)
+		return (0);
+	else
+		return (1);
 }
